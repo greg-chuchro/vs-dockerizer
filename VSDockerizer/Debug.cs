c21c284fdccff4b22abee015544c64a1294fde40
greg-chuchro <gchuchro@acm.org>
2021-04-12 13:15:45 +0900

    #5 debug launch

--- VSDockerizer/Debug.cs
+++ VSDockerizer/Debug.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.ComponentModel.Design;
-using System.Globalization;
-using System.Threading;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using Task = System.Threading.Tasks.Task;
+using EnvDTE;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace VSDockerizer {
     /// <summary>
@@ -27,15 +28,19 @@ namespace VSDockerizer {
         /// </summary>
         private readonly AsyncPackage package;
 
+        private readonly DTE dte;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Debug"/> class.
         /// Adds our command handlers for menu (commands must exist in the command table file)
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
         /// <param name="commandService">Command service to add command to, not null.</param>
-        private Debug(AsyncPackage package, OleMenuCommandService commandService) {
+        private Debug(AsyncPackage package, OleMenuCommandService commandService, DTE dte) {
             this.package = package ?? throw new ArgumentNullException(nameof(package));
             commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            this.dte = dte;
 
             var menuCommandID = new CommandID(CommandSet, CommandId);
             var menuItem = new MenuCommand(this.Execute, menuCommandID);
@@ -69,7 +74,9 @@ namespace VSDockerizer {
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
             OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-            Instance = new Debug(package, commandService);
+            var dte = await package.GetServiceAsync(typeof(DTE)) as DTE ?? throw new ArgumentNullException("dte is null");
+            dte.ExecuteCommand("DebugAdapterHost.Logging", "/On /OutputWindow");
+            Instance = new Debug(package, commandService, dte);
         }
 
         /// <summary>
@@ -81,17 +88,24 @@ namespace VSDockerizer {
         /// <param name="e">Event args.</param>
         private void Execute(object sender, EventArgs e) {
             ThreadHelper.ThrowIfNotOnUIThread();
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "Debug";
 
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.package,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("VSDockerizer.Resources.launch.json"))
+            using (var reader = new StreamReader(stream)) {
+                var launchJson = reader.ReadToEnd();
+                dynamic launch = JObject.Parse(launchJson);
+                var project = (dte.ActiveSolutionProjects as object[])[0] as Project;
+                var dllName = project.Properties.Item("AssemblyName").Value;
+                launch.adapterArgs = string.Format((string)launch.adapterArgs, project.Name);
+                launch.configurations[0].program = string.Format((string)launch.configurations[0].program, dllName);
+
+                var launchFileName = "launch.json";
+                var launchFilePath = Path.Combine(Path.GetDirectoryName(dte.Solution.FullName), ".vs", project.Name, launchFileName);
+                using (var file = File.CreateText(launchFilePath)) {
+                    file.Write(JsonConvert.SerializeObject(launch));
+                }
+
+                dte.ExecuteCommand("DebugAdapterHost.Launch", $"/LaunchJson:\"{launchFilePath}\"");
+            }
         }
     }
 }
